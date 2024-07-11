28f591cfde106ce99f1da92e15363466a6a1fcda
JennyLottner <jenny.lottner@gmail.com>
2024-07-11 20:02:04 +0300

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  // import AppHeader from './cmps/AppHeader.vue'
+  // import AppFooter from './cmps/AppFooter.vue'
+
+export default {
+  components: {
+    // AppHeader,
+    // AppFooter
+  }
+}
+</script>
+
+<template>
+  <!-- <AppHeader /> -->
+  <RouterView />
+  <!-- <AppFooter /> -->
+</template>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '../pages/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
+    {
+      path: '/contacts',
+      name: 'contacts',
+      component: HomePage
+    },
+    {
+      path: '/stats',
+      name: 'stats',
+      component: HomePage
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: HomePage
+    },
+  ]
+})
+
+export default router
