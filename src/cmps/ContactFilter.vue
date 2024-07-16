67affa1acff348a8bd50f4cc2beab19c194f7616
JennyLottner <jenny.lottner@gmail.com>
2024-07-17 00:23:25 +0300

    Add debounce to contact filter

--- src/cmps/ContactFilter.vue
+++ src/cmps/ContactFilter.vue
@@ -1,29 +1,34 @@
 <script>
-export default {
-    data() { return {
-        filterBy: null,
-    }},
+import { utilService } from '@/services/util.service'
 
-    methods: {
-        onSearch() {
-            this.$emit('loadContacts', this.filterBy)
-        },
-    },
+export default {
+  data() {
+    return {
+      filterBy: null,
+    }
+  },
+
+  methods: {
+    onSearch() {
+      utilService.debounce(() => {
+        this.$emit("loadContacts", this.filterBy);
+      }, 300).call(this);
+    }
+  },
 
 }
 </script>
 
 <template>
-    <fieldset class="contacts-filter">
-        <legend>Filter</legend>
-        <input type="text" @input="onSearch" v-model="filterBy">
-    </fieldset>
-  
+  <fieldset class="contacts-filter">
+    <legend>Filter</legend>
+    <input type="text" @input="onSearch" v-model="filterBy" />
+  </fieldset>
 </template>
 
 <style lang="scss">
 .contacts-filter {
-    margin-block: 10px 20px;
-    padding-block: 0;
+  margin-block: 10px 20px;
+  padding-block: 0;
 }
 </style>
