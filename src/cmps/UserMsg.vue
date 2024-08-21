f0e670a66b10099ef1b4c415a372ecda274c77da
JennyLottner <jenny.lottner@gmail.com>
2024-08-21 21:06:23 +0300

    Add usermsg and eventBus

--- src/cmps/UserMsg.vue
+++ src/cmps/UserMsg.vue
@@ -1,60 +1,60 @@
 <script>
-import { eventBusService } from '@/services/eventBus.service.js'
+import { eventBus } from '@/services/eventBus.service.js'
 
 export default {
-	data() {
-		return {
-			msg: { txt: '', type: 'success', timeout: 1500 },
-		}
-	},
-	methods: {
-		showMsg(msg) {
-			this.msg = msg
-			setTimeout(() => (this.msg.txt = ''), this.msg.timeout || 1500)
-		},
-	},
-	created() {
-		eventBusService.on('show-user-msg', this.showMsg)
-	},
+  data() {
+    return {
+      msg: { txt: '', type: 'success' },
+    }
+  },
+  methods: {
+    showMsg(msg) {
+      this.msg = msg
+      setTimeout(() => this.msg.txt = '', 2000000)
+    },
+  },
+  created() {
+    this.unsubscribe = eventBus.on('user-msg', this.showMsg)
+  },
+  unmounted() { this.unsubscribe() },
 }
 </script>
 
 <template>
-    <Transition>
-        <section v-if="msg.txt" class="user-msg">
-            <p :class="msg.type">{{ msg.txt }}</p>
-        </section>
-    </Transition>
+  <Transition>
+    <section v-if="msg.txt" class="user-msg" :class="msg.type">
+      <p>{{ msg.txt }}</p>
+    </section>
+  </Transition>
 </template>
 
 <style lang="scss">
+.user-msg {
+  position: fixed;
+  bottom: 40px;
+  right: 20px;
+
+  padding: 1rem;
+  min-width: 200px;
+  border-radius: 5px;
+  
+  &.success {
+    background-color: rgb(24, 198, 125);
+  }
+
+  &.error {
+    background-color: rgb(253, 122, 122);
+  }
+}
+
 .v-enter-active,
 .v-leave-active {
-	transition: 0.5s ease;
+  transition: 0.5s ease;
 }
 
 .v-enter-from,
 .v-leave-to {
-    translate: 200%;
-	opacity: 0;
-}
-
-.user-msg {
-	position: absolute;
-	bottom: 20px;
-	right: 20px;
-
-	p {
-		padding: 10px;
-		border-radius: 10px;
-	}
-}
-
-.success {
-	background-color: lightgreen;
-}
-
-.error {
-	background-color: orangered;
+  translate: 200%;
+  opacity: 0;
 }
 </style>
